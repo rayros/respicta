e5b0930a74b3a946e22aa3f5ec0b1b2bd2b719c4
Paweł Łaski <pawel.laski@bacca.pl>
2024-03-02 21:09:28 +0100

    Added lib oxipng

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use oxipng::{optimize, InFile, Options, OutFile};
+
 fn main() {
-    println!("Hello, world!");
+    // Specify the input PNG file path
+    let input_png_path = "tests/files/issue-159.png";
+
+    // Specify the output PNG file path (optional)
+    let output_png_path = "output-image.png";
+
+    // Set up the optimization options
+    let options = Options {
+        strip: oxipng::StripChunks::Safe, // Optionally, strip metadata
+        ..Options::default()
+    };
+
+    // Perform PNG optimization
+    match optimize(
+        &input_png_path.into(),
+        &OutFile::from_path(output_png_path.into()),
+        &options,
+    ) {
+        Ok(_) => {
+            println!("Oxipng optimization successful!");
+        }
+        Err(e) => {
+            eprintln!("Error running oxipng: {:?}", e);
+        }
+    }
 }
