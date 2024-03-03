0aeb3e7b99d887eec1639bdddc753676f45cf37a
Paweł Łaski <pawel.laski@bacca.pl>
2024-03-03 20:41:00 +0100

    Move optimize oxipng to module

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,8 @@
-use oxipng::{optimize, InFile, Options, OutFile};
+mod oxipng;
+
+// use oxipng::optimize;
 
 fn main() {
     // Specify the input PNG file path
-    let input_png_path = "tests/files/issue-159.png";
-
-    // Specify the output PNG file path (optional)
-    let output_png_path = "output-image.png";
-
-    // Set up the optimization options
-    let options = Options {
-        strip: oxipng::StripChunks::Safe, // Optionally, strip metadata
-        ..Options::default()
-    };
-
-    // Perform PNG optimization
-    match optimize(
-        &input_png_path.into(),
-        &OutFile::from_path(output_png_path.into()),
-        &options,
-    ) {
-        Ok(_) => {
-            println!("Oxipng optimization successful!");
-        }
-        Err(e) => {
-            eprintln!("Error running oxipng: {:?}", e);
-        }
-    }
+    // optimize(input_png_path, output_png_path)
 }
---
+++ src/oxipng.rs
@@ -0,0 +1,31 @@
+use oxipng::{Options, OutFile};
+
+pub fn optimize(input_png_path: &str, output_png_path: &str) -> Result<(), oxipng::PngError> {
+    // Set up the optimization options
+    let options = Options {
+        strip: oxipng::StripChunks::Safe, // Optionally, strip metadata
+        ..Options::default()
+    };
+
+    // Perform PNG optimization
+    oxipng::optimize(
+        &input_png_path.into(),
+        &OutFile::from_path(output_png_path.into()),
+        &options,
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn oxipng_optimize() -> Result<(), oxipng::PngError> {
+        use super::*;
+        // Specify the input PNG file path
+        let input_png_path = "tests/files/issue-159.png";
+
+        // Specify the output PNG file path (optional)
+        let output_png_path = "target/issue-159.png";
+
+        optimize(input_png_path, output_png_path)
+    }
+}
