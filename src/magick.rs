e36650a252a00e47c907da0adf640c027fb00aac
Paweł Łaski <pawel.laski@bacca.pl>
2024-03-08 04:07:46 +0100

    Removed mozjpeg - magick_rust conflicts

--- src/magick.rs
+++ src/magick.rs
@@ -1,8 +1,8 @@
-use magick_rust::{magick_wand_genesis, MagickWand};
 use std::sync::Once;
 
 // Used to make sure MagickWand is initialized exactly once. Note that we
 // do not bother shutting down, we simply exit when we're done.
+
 static START: Once = Once::new();
 
 pub fn resize_and_auto_orient(
@@ -11,27 +11,42 @@ pub fn resize_and_auto_orient(
     target_width: usize,
     target_height: usize,
 ) -> Result<(), magick_rust::MagickError> {
+    use magick_rust::{magick_wand_genesis, MagickWand};
+
     START.call_once(|| {
         magick_wand_genesis();
     });
-    let wand = MagickWand::new();
+    let mut wand = MagickWand::new();
     wand.read_image(input_jpg_path)?;
     wand.auto_orient();
+    wand.strip_image()?;
     wand.fit(target_width, target_height);
+    wand.set_image_compression_quality(75)?;
     wand.write_image(output_jpg_path)
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn magick_resize_and_auto_orient() -> Result<(), magick_rust::MagickError> {
+    fn magic_resize_and_auto_orient() -> Result<(), magick_rust::MagickError> {
         use super::*;
         // Specify the input PNG file path
-        let input_jpg_path = "tests/files/orientation_test.jpg";
+        let input_jpg_path = "tests/files/img20230418182427.jpg";
 
         // Specify the output PNG file path (optional)
-        let output_png_path = "target/out_image_1.jpg";
+        let output_png_path = "target/magick_out.jpg";
 
-        resize_and_auto_orient(input_jpg_path, output_png_path, 240, 0)
+        resize_and_auto_orient(input_jpg_path, output_png_path, 240, 100)
+    }
+    #[test]
+    fn magic_resize_and_auto_orient_gif() -> Result<(), magick_rust::MagickError> {
+        use super::*;
+        // Specify the input PNG file path
+        let input_jpg_path = "tests/files/test1.gif";
+
+        // Specify the output PNG file path (optional)
+        let output_png_path = "target/magick_out.gif";
+
+        resize_and_auto_orient(input_jpg_path, output_png_path, 500, 0)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
+use std::ffi::CString;
+
 mod magick;
-mod mozjpeg;
 mod oxipng;
 mod webp;
-// use oxipng::optimize;
+use oxipng::optimize;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    magick::resize_and_auto_orient(
+        "tests/files/img20230418182427.jpg",
+        "target/magick_out_image_1.jpg",
+        240,
+        100,
+    )?;
+    let args: Vec<_> = std::env::args()
+        .map(|arg| CString::new(arg).unwrap())
+        .collect();
+    let argv: Vec<_> = args.iter().map(|a| a.as_ptr()).collect();
+
+    unsafe {
+        gifsicle::gifsicle_main(argv.len() as _, argv.as_ptr());
+    }
+
     // Specify the input PNG file path
     // optimize(input_png_path, output_png_path)
+
+    Ok(())
 }
