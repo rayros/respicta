230e1ec320f0dda5b4e070a3ebb1ca7d790baf2e
Paweł Łaski <pawel.laski@bacca.pl>
2024-03-20 20:16:58 +0100

    Refactor CLI structure and add image conversion subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,52 @@
 use image_resizer::utils::gifsicle;
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let input_path = "tests/files/test1.gif";
 
-    // Specify the output PNG file path (optional)
-    let output_path = "target/gifsicle_test1.gif";
+use std::path::PathBuf;
 
-    gifsicle::optimize(gifsicle::Config {
-        input_path,
-        output_path,
-        width: Some(100),
-        height: Some(100),
-    });
+use clap::{Parser, Subcommand};
 
-    Ok(())
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    #[clap(
+        disable_help_flag = true,
+        after_help = "
+Examples:
+
+  image-resizer convert --width 100 --height 100 input.jpg output.jpg
+
+"
+    )]
+    /// Convert images from one format to another
+    Convert {
+        #[clap(long, action = clap::ArgAction::HelpLong)]
+        help: Option<bool>,
+        /// Input image path
+        input_path: String,
+        /// Output image path
+        output_path: String,
+        /// Width of the output image
+        /// If not set, the width will be the same as the input image
+        #[clap(short, long)]
+        width: Option<u32>,
+        /// Height of the output image
+        /// If not set, the height will be the same as the input image
+        #[clap(short, long)]
+        height: Option<u32>,
+    },
+}
+
+fn main() {
+    let cli = Cli::parse();
+}
+
+#[test]
+fn verify_cli() {
+    use clap::CommandFactory;
+    Cli::command().debug_assert();
 }
