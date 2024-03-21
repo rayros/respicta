2c0e165ebb695f000a1e8ad21a3f011acd4b3b72
Paweł Łaski <pawel.laski@bacca.pl>
2024-03-21 22:03:10 +0100

    Add image file extensions

---
+++ src/extensions.rs
@@ -0,0 +1,6 @@
+pub const GIF: &str = "gif";
+pub const PNG: &str = "png";
+pub const WEBP: &str = "webp";
+pub const JPG: &str = "jpg";
+pub const JPEG: &str = "jpeg";
+pub const JFIF: &str = "jfif";
