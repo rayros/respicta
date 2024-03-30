879dfdc1607c9f01b4de553c2684fe217fc99bd3
Paweł Łaski <pawel.laski@bacca.pl>
2024-03-30 15:28:48 +0100

    Optimize image resizing and add clippy lint allowance

--- src/utils/magick.rs
+++ src/utils/magick.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::cast_precision_loss)]
+
 use magick_rust::{magick_wand_genesis, MagickWand};
 use std::{path::PathBuf, sync::Once};
 
@@ -25,9 +27,27 @@ pub fn optimize(config: &Config) -> Result<(), magick_rust::MagickError> {
         .height
         .map_or(wand.get_image_height(), |s| s as usize);
 
-    wand.fit(width, height);
     wand.auto_orient();
     wand.strip_image()?;
+
+    let mut width_ratio = width as f64;
+    width_ratio /= wand.get_image_width() as f64;
+    let mut height_ratio = height as f64;
+    height_ratio /= wand.get_image_height() as f64;
+    let (new_width, new_height) = if width_ratio < height_ratio {
+        (
+            width,
+            (wand.get_image_height() as f64 * width_ratio) as usize,
+        )
+    } else {
+        (
+            (wand.get_image_width() as f64 * height_ratio) as usize,
+            height,
+        )
+    };
+
+    wand.adaptive_resize_image(new_width, new_height)?;
+
     wand.set_image_compression_quality(75)?;
 
     wand.write_image(&config.output_path.display().to_string())
