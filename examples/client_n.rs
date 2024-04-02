17cd49674ccdc7142870e6e20df16306fec4d74b
Paweł Łaski <pawel.laski@bacca.pl>
2024-04-02 02:21:49 +0200

    Add client_n.rs with code to send multiple requests

---
+++ examples/client_n.rs
@@ -0,0 +1,24 @@
+async fn send_request() {
+    let multipart = reqwest::multipart::Form::new().part(
+        "file",
+        reqwest::multipart::Part::bytes(
+            include_bytes!("../tests/files/convert_test1.JPG").to_vec(),
+        )
+        .file_name("convert_test1.JPG"),
+    );
+    reqwest::Client::new()
+        .post("http://localhost:3000/")
+        .header("extension", "webp")
+        .multipart(multipart)
+        .send()
+        .await
+        .unwrap();
+}
+
+#[tokio::main]
+async fn main() {
+    // send request to server n times
+    for _ in 0..100 {
+        send_request().await;
+    }
+}
