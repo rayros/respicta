a404989f94775f12f4d9397b948e17f0069cc114
Paweł Łaski <pawel.laski@bacca.pl>
2024-05-10 18:39:20 +0200

    refactor: Add #[must_use] attribute to fit function in utils module

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -3,6 +3,7 @@ pub mod magick;
 pub mod oxipng;
 pub mod webp;
 
+#[must_use]
 pub fn fit(width: u32, height: u32, max_width: u32, max_height: u32) -> (u32, u32) {
     let width_ratio = f64::from(max_width) / f64::from(width);
     let height_ratio = f64::from(max_height) / f64::from(height);
