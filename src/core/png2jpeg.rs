7e759b364b0be9bfd11ff5fcf09fe91807331ba0
Paweł Łaski <zniwus@gmail.com>
2025-11-28 22:19:30 +0100

    Refactor error handling in conversion functions: unify error types across JPEG and PNG conversions, and enhance error reporting for HEIC and WebP conversions

--- src/core/png2jpeg.rs
+++ src/core/png2jpeg.rs
@@ -1,14 +1,30 @@
 use crate::{utils::magick, Dimensions, PathAccessor, Quality};
 
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("Magick error: {0}")]
+    Magick(magick::Error),
+}
+
+impl From<magick::Error> for Error {
+    fn from(err: magick::Error) -> Self {
+        Error::Magick(err)
+    }
+}
+
 /// # Errors
 ///
 /// Returns an error if the conversion fails.
 ///
-pub fn convert<T>(config: &T) -> std::result::Result<(), magick::Error>
+pub fn convert<T>(config: &T) -> std::result::Result<(), Error>
 where
     T: PathAccessor + Dimensions + Quality,
 {
-    magick::optimize(config, None)
+    magick::optimize(config, None)?;
+
+    Ok(())
 }
 
 #[cfg(test)]
